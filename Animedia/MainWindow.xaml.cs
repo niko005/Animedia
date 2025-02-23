769756d23675ebb1a0a5ea269887ea4179881ee4
Nikamagomed <abdull89280622251@gmail.com>
2025-02-23 15:45:07 +0300

    Rename Animelist-Animedia

---
+++ Animedia/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Animedia
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                login_btn_Click(sender, e);
+        }
+
+        private void login_btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
